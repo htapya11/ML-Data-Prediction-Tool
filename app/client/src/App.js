234121c790b6ab9a20ae1493f4d849ff9fb36693
Huzefa Tapya <tapyahuzefa@gmail.com>
2024-12-22 17:26:59 -0500

    Add form

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./app.scss"
+import {
+  Form,
+  FormGroup,
+  Select,
+  SelectItem,
+  Button,
+  Loading
+} from "carbon-components-react"
+import {years, months, costCenters, accounts} from "./utils"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="mainContainer">
+        <Form>
+          <FormGroup>
+            <Select id="select-0" labelText="Select Year">
+              {years.map((yearVal => (<SelectItem text={yearVal} value={yearVal} />
+              )))}
+            </Select>
+          </FormGroup>
+        </Form>
+        <Form>
+          <FormGroup>
+            <Select id="select-1" labelText="Select Month">
+              {months.map((monthVal => (<SelectItem text={monthVal} value={monthVal} />
+              )))}
+            </Select>
+          </FormGroup>
+        </Form>
+        <Form>
+          <FormGroup>
+            <Select id="select-2" labelText="Select Cost Center">
+              {costCenters.map((costCentersVal => (<SelectItem text={costCentersVal} value={costCentersVal} />
+              )))}
+            </Select>
+          </FormGroup>
+        </Form>
+        <Form>
+          <FormGroup>
+            <Select id="select-3" labelText="Select Account">
+              {accounts.map((accountVal => (<SelectItem text={accountVal} value={accountVal} />
+              )))}
+            </Select>
+          </FormGroup>
+          <Button>Predict</Button>
+        </Form>
       </header>
     </div>
   );
---
+++ app/client/src/utils.js
@@ -0,0 +1,20 @@
+export const years = [2019, 2020, 2021]
+
+export const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+]
+
+export const costCenters = []
+
+export const accounts = []
