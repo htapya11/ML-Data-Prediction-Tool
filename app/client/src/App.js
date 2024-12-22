f006d834a9367da647d3f83a522bc54ddc2b1360
Huzefa Tapya <tapyahuzefa@gmail.com>
2024-12-22 17:49:26 -0500

    Add prediction function

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -8,13 +8,38 @@ import {
   Loading
 } from "carbon-components-react"
 import {years, months, costCenters, accounts} from "./utils"
+import { useState } from "react";
+import axios from "axios"
 function App() {
+
+  const [year, setYear] = useState(2020)
+  const [month, setMonth] = useState("Jan")
+  const [costCenter, setCostCenter] = useState("CC101")
+  const [account, setAccount] = useState(1000000)
+  const [prediction, setPrediction] = useState()
+  const [scores, setScores] = useState([])
+
+  const runPred = async(year, month, costCenter, account) => {
+    setPrediction("Scoring")
+    const res = await axios.post("/api/wml/score", {
+      year,
+      month,
+      costCenter,
+      account,
+    })
+    setPrediction(res.data.values[0])
+    setScores([...scores,
+      {group:year+month+costCenter+account, value:res.data.values[0][0]},
+    ])
+    console.log(res)
+  }
+
   return (
     <div className="App">
       <header className="mainContainer">
         <Form>
           <FormGroup>
-            <Select id="select-0" labelText="Select Year">
+            <Select id="select-0" labelText="Select Year" onChange={e=>setYear(e.target.value)}>
               {years.map((yearVal => (<SelectItem text={yearVal} value={yearVal} />
               )))}
             </Select>
@@ -22,7 +47,7 @@ function App() {
         </Form>
         <Form>
           <FormGroup>
-            <Select id="select-1" labelText="Select Month">
+            <Select id="select-1" labelText="Select Month" onChange={e=>setMonth(e.target.value)}>
               {months.map((monthVal => (<SelectItem text={monthVal} value={monthVal} />
               )))}
             </Select>
@@ -30,7 +55,7 @@ function App() {
         </Form>
         <Form>
           <FormGroup>
-            <Select id="select-2" labelText="Select Cost Center">
+            <Select id="select-2" labelText="Select Cost Center" onChange={e=>setCostCenter(e.target.value)}>
               {costCenters.map((costCentersVal => (<SelectItem text={costCentersVal} value={costCentersVal} />
               )))}
             </Select>
@@ -38,13 +63,20 @@ function App() {
         </Form>
         <Form>
           <FormGroup>
-            <Select id="select-3" labelText="Select Account">
+            <Select id="select-3" labelText="Select Account" onChange={e=>setAccount(e.target.value)}>
               {accounts.map((accountVal => (<SelectItem text={accountVal} value={accountVal} />
               )))}
             </Select>
           </FormGroup>
-          <Button>Predict</Button>
+          <Button onChange={e=>runPred(year, month, costCenter, account)}>Predict</Button>
         </Form>
+        <div className="predictionContainer">
+          {prediction !=="Scoring" && prediction ? 
+          "The model predicted":""}
+        <div className="predictionResult">
+          {prediction=="Scoring" ? (<Loading description="Loading..." />) : !prediction ? "" : parseInt(prediction)}
+        </div>
+        </div>
       </header>
     </div>
   );
